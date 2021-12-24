163615778bd057d3b164349c489bbff9999cd4c5
Ranaihab <rana.ihab7@gmail.com>
2021-12-24 13:28:03 +0200

    Change AGAT

--- src/AGAT.java
+++ src/AGAT.java
@@ -20,9 +20,9 @@ public class AGAT {
             if (allProcesses.get(i).getArrivalTime() > v1)
                 v1 = allProcesses.get(i).getArrivalTime();
         }
-        if(v1 < 10){
+        if (v1 < 10) {
             v1 = 1;
-        }else{
+        } else {
             v1 /= 10;
         }
         readyQueue = new ArrayList<>();
@@ -49,121 +49,127 @@ public class AGAT {
         inExecution.quantum = 0;
         inExecution.computeWaitingTurnaroundTime();
         finishedProcesses.add(inExecution);
-        readyQueue.remove(inExecution);
     }
 
-    public void computeV2(){
-        v2 = 1;
-        for(int i =0; i< readyQueue.size(); i++){
-            if(readyQueue.get(i).getBurstTime() > v2){
+    public void computeV2(Process inExecution) {
+        v2 = inExecution.getBurstTime();
+        for (int i = 0; i < readyQueue.size(); i++) {
+            if (readyQueue.get(i).getBurstTime() > v2) {
                 v2 = readyQueue.get(i).getBurstTime();
             }
         }
-        for(int i =0; i< allProcesses.size(); i++){
-            if(allProcesses.get(i).getBurstTime() > v2){
+        for (int i = 0; i < allProcesses.size(); i++) {
+            if (allProcesses.get(i).getBurstTime() > v2) {
                 v2 = allProcesses.get(i).getBurstTime();
             }
         }
-        if(v2 < 10){
+        if (v2 < 10) {
             v2 = 1;
-        }else{
+        } else {
             v2 /= 10;
         }
     }
 
-    public void computeFactor(){
-        computeV2();
-        for (int i =0; i< readyQueue.size(); i++){
-            Process p =readyQueue.get(i);
-            p.factor = (10 - p.getPriority()) + (int)Math.ceil(p.getArrivalTime()/v1) + (int)Math.ceil(p.getBurstTime()/v2);
+    public void computeFactor(Process inExecution) {
+        computeV2(inExecution);
+        for (int i = 0; i < readyQueue.size(); i++) {
+            Process p = readyQueue.get(i);
+            p.factor = (10 - p.getPriority()) + (int) Math.ceil(p.getArrivalTime() / v1) + (int) Math.ceil(p.getBurstTime() / v2);
             p.AGAT.add(p.factor);
         }
-        for (int i =0; i< allProcesses.size(); i++){
-            Process p =allProcesses.get(i);
-            p.factor = (10 - p.getPriority()) + (int)Math.ceil(p.getArrivalTime()/v1) + (int)Math.ceil(p.getBurstTime()/v2);
+        for (int i = 0; i < allProcesses.size(); i++) {
+            Process p = allProcesses.get(i);
+            p.factor = (10 - p.getPriority()) + (int) Math.ceil(p.getArrivalTime() / v1) + (int) Math.ceil(p.getBurstTime() / v2);
             p.AGAT.add(p.factor);
         }
+        inExecution.factor = (10 - inExecution.getPriority()) + (int) Math.ceil(inExecution.getArrivalTime() / v1) + (int) Math.ceil(inExecution.getBurstTime() / v2);
+        inExecution.AGAT.add(inExecution.factor);
     }
 
     //Returns process that has minimum AGAT factor
-    public Process getMinFactor(){
-        if(readyQueue.size() == 0)
+    public Process getMinFactor() {
+        if (readyQueue.size() == 0)
             return null;
         Process minimum = readyQueue.get(0);
-        for (int i =1; i< readyQueue.size(); i++){
-            if(readyQueue.get(i).factor < minimum.factor)
+        for (int i = 1; i < readyQueue.size(); i++) {
+            if (readyQueue.get(i).factor < minimum.factor)
                 minimum = readyQueue.get(i);
         }
         return minimum;
     }
 
     //Selects next process to execute
-    public Process executeProcess(){
+    public Process executeProcess() {
         refill();
         return getMinFactor();
 
     }
 
     //Replace process because it finished its quantum time
-    public Process replaceProcess(Process inExecution){
+    public Process replaceProcess(Process inExecution) {
         inExecution.end.add(time);
-        Process temp = executeProcess();
-        inExecution.setBurstTime(inExecution.getBurstTime() - inExecution.quantum);
-        inExecution.quantumTime.add(inExecution.quantum+2);
-        inExecution.quantum+=2;
+        refill();
         readyQueue.add(inExecution);
-        if(temp == null)
-            temp = inExecution;
-        readyQueue.remove(temp);
+        Process temp = readyQueue.get(0);
+        inExecution.setBurstTime(inExecution.getBurstTime() - inExecution.quantum);
+        inExecution.quantumTime.add(inExecution.quantum + 2);
+        inExecution.quantum += 2;
         inExecution = temp;
+        readyQueue.remove(inExecution);
         inExecution.start.add(time);
         return inExecution;
     }
 
     public void AGAlgorithm() {
-        computeFactor();
-        int i =0;
-        Process inExecution = executeProcess();
+        refill();
+        Process inExecution = readyQueue.get(0);
+        computeFactor(inExecution);
+        int i = 0;
+        readyQueue.remove(0);
         processExecutionOrder.add(inExecution.getName());
         int quantum40 = (int) Math.round(inExecution.quantum * 40 / 100.0);
         inExecution.start.add(time);
-        while (readyQueue.size() != 0) {
+        while (true) {
 
-            if(i != 0) {
-                computeFactor();
+            if (i != 0) {
+                computeFactor(inExecution);
                 processExecutionOrder.add(inExecution.getName());
             }
             i++;
 
             if (inExecution.getBurstTime() <= quantum40) {
                 endExecution(inExecution);
-                if(readyQueue.size()==0)
+                refill();
+                if (readyQueue.size() == 0)
                     break;
-                inExecution = executeProcess();
+                inExecution = readyQueue.get(0);
+                readyQueue.remove(0);
                 inExecution.start.add(time);
                 quantum40 = (int) Math.round(inExecution.quantum * 40.0 / 100.0);
 
-            }else if(quantum40 == inExecution.quantum){
-                time+=quantum40;
-                readyQueue.remove(inExecution);
+
+            } else if (quantum40 == inExecution.quantum) {
+                time += quantum40;
+
                 inExecution = replaceProcess(inExecution);
-                readyQueue.add(inExecution);
                 quantum40 = (int) Math.round(inExecution.quantum * 40 / 100.0);
             } else {
-                time+=quantum40;
+                time += quantum40;
                 Process temp = executeProcess();
-                if(temp == inExecution || temp == null){
-                    time-=quantum40;
+//                readyQueue.remove(inExecution);
+//                readyQueue.add(inExecution);
+                if (temp == null || (temp!=null && temp.factor >= inExecution.factor )) {
+                    time -= quantum40;
                     quantum40++;
-                    i =0;
-                }else{
-                    inExecution.setBurstTime(inExecution.getBurstTime()-quantum40);
-                    inExecution.quantum+=(inExecution.quantum-quantum40);
+                    i = 0;
+                } else {
+                    inExecution.setBurstTime(inExecution.getBurstTime() - quantum40);
+                    inExecution.quantum += (inExecution.quantum - quantum40);
                     inExecution.quantumTime.add(inExecution.quantum);
                     inExecution.end.add(time);
-                    readyQueue.remove(inExecution);
                     readyQueue.add(inExecution);
                     inExecution = temp;
+                    readyQueue.remove(inExecution);
                     quantum40 = (int) Math.round(inExecution.quantum * 40.0 / 100.0);
                     inExecution.start.add(time);
                 }
@@ -171,15 +177,15 @@ public class AGAT {
         }
     }
 
-    public void printInfo(){
+    public void printInfo() {
         double waitingTime = 0;
-        double turnaround =0;
-        for(int i =0; i< finishedProcesses.size(); i++){
+        double turnaround = 0;
+        for (int i = 0; i < finishedProcesses.size(); i++) {
             waitingTime += finishedProcesses.get(i).waitingTime;
             turnaround += finishedProcesses.get(i).getTurnaroundTime();
         }
         System.out.println("Process execution order: ");
-        for (int i =0; i<processExecutionOrder.size(); i++)
+        for (int i = 0; i < processExecutionOrder.size(); i++)
             System.out.print(processExecutionOrder.get(i) + " ");
         System.out.println();
         waitingTime /= finishedProcesses.size();
