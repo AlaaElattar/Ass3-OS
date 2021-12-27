287402b20d39c8e8167713e39d5f1d3369727375
Ranaihab <rana.ihab7@gmail.com>
2021-12-27 20:07:44 +0200

    .

--- src/SJF.java
+++ src/SJF.java
@@ -2,44 +2,44 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class SJF {
-    private ArrayList < Process > processes;
-    private ArrayList < Integer > executedProcesses = new ArrayList < Integer > ( );
+    private ArrayList<Process> processes;
+    private ArrayList<Integer> executedProcesses = new ArrayList<Integer>();
 
-    SJF ( ArrayList < Process > processes ) {
+    SJF(ArrayList<Process> processes) {
         this.processes = processes;
     }
 
-    public void startScheduling ( ) {
+    public void startScheduling() {
         int currTime = 1;
         int currProcess = 0;
-        for ( int i = 0 ; i < processes.size ( ) ; i++ ) {
+        for (int i = 0; i < processes.size(); i++) {
             int temp = currProcess;
             do {
-                currProcess = getNextProcessNumber ( currTime );
-                if ( currProcess == - 1 ) {
+                currProcess = getNextProcessNumber(currTime);
+                if (currProcess == -1) {
                     currTime++;
                 }
-            } while ( currProcess == - 1 );
+            } while (currProcess == -1);
 
-            processes.get ( currProcess ).Execute ( );
-            processes.get ( currProcess ).setWaitingTime ( currTime - processes.get ( currProcess ).getArrivalTime ( ) );
-            processes.get ( currProcess ).setTurnaroundTime ( processes.get ( currProcess ).getWaitingTime ( ) + processes.get ( currProcess ).getBurstTime ( ) );
-            currTime += processes.get ( currProcess ).getBurstTime ( );
-            executedProcesses.add ( currProcess );
+            processes.get(currProcess).Execute();
+            processes.get(currProcess).setWaitingTime(currTime - processes.get(currProcess).getArrivalTime());
+            processes.get(currProcess).setTurnaroundTime(processes.get(currProcess).getWaitingTime() + processes.get(currProcess).getBurstTime());
+            currTime += processes.get(currProcess).getBurstTime();
+            executedProcesses.add(currProcess);
             aging(currTime);
 
         }
     }
 
-    private int getNextProcessNumber ( int currTime ) {
-        int nextProcessNumber = - 1;
-        for ( int i = 0 ; i < processes.size ( ) ; i++ ) {
-            if ( ! executedProcesses.contains ( i ) && processes.get ( i ).getArrivalTime ( ) <= currTime ) { // the next process must be arrived and must have
+    private int getNextProcessNumber(int currTime) {
+        int nextProcessNumber = -1;
+        for (int i = 0; i < processes.size(); i++) {
+            if (!executedProcesses.contains(i) && processes.get(i).getArrivalTime() <= currTime) { // the next process must be arrived and must have
                 // the lest BurstTime
-                if ( nextProcessNumber == - 1 ) {
+                if (nextProcessNumber == -1) {
                     nextProcessNumber = i;
                 } // if it's the first process
-                else if ( processes.get ( i ).processingTime< processes.get ( nextProcessNumber ).processingTime ) {
+                else if (processes.get(i).processingTime < processes.get(nextProcessNumber).processingTime) {
                     nextProcessNumber = i;
                 }
             }
@@ -47,28 +47,28 @@ public class SJF {
         return nextProcessNumber;
     }
 
-    public void aging(int currTime){
-        for(int i =0; i<processes.size(); i++){
-            if(processes.get(i).getArrivalTime()<=currTime)
+    public void aging(int currTime) {
+        for (int i = 0; i < processes.size(); i++) {
+            if (processes.get(i).getArrivalTime() <= currTime && processes.get(i).processingTime != 0)
                 processes.get(i).processingTime -= 1;
         }
     }
 
-    public double getAverageWaitingTime ( ) {
+    public double getAverageWaitingTime() {
         double sumOfWaiting = 0.0;
-        for ( Process p : processes ) {
-            sumOfWaiting += p.getWaitingTime ( );
+        for (Process p : processes) {
+            sumOfWaiting += p.getWaitingTime();
         }
-        return sumOfWaiting / processes.size ( );
+        return sumOfWaiting / processes.size();
     }
 
-    public double getAverageTurnaroundTime ( ) {
+    public double getAverageTurnaroundTime() {
         double sumOfTurnAround = 0.0;
-        for ( Process p : processes ) {
-            sumOfTurnAround += p.getTurnaroundTime ( );
+        for (Process p : processes) {
+            sumOfTurnAround += p.getTurnaroundTime();
         }
 
-        return sumOfTurnAround / processes.size ( );
+        return sumOfTurnAround / processes.size();
     }
 
 }
